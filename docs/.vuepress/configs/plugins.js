6f776f0162049bcf7716f4402c33c7199faefaff
Roy_Kid <lijichen365@126.com>
2020-03-01 20:47:41 +0800

    v 0.0.2 add plugin; add content; bug fix

--- docs/.vuepress/configs/plugins.js
+++ docs/.vuepress/configs/plugins.js
@@ -1,4 +1,19 @@
 module.exports = [
     ['@vuepress/back-to-top', true],
-    ['vuepress-plugin-flowchart', true]
+    ['vuepress-plugin-flowchart', true],
+    ['vuepress-plugin-baidu-autopush', true],
+    ['vuepress-plugin-baidu-google-analytics',{
+        hm: '915d9a17e5448406c16519f87d253e84',
+        ga: 'UA-159337280-1'
+    }],
+    ['@vuepress/active-header-links', {
+        sidebarLinkSelector: '.sidebar-link',
+        headerAnchorSelector: '.header-anchor'
+    }],
+    // vuepress-plugin-copyright
+    ['vuepress-plugin-sitemap', {
+        hostname: 'https://lammps.org.cn'
+    }],
+
+    ['vuepress-plugin-smooth-scroll', true]
 ]
