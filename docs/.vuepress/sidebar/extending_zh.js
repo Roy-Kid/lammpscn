8c3ea155c1561e255d9dd0eb7cf062acca49bc32
Roy Kid <lijichen365@126.com>
2021-11-25 00:24:56 +0800

    fix bug

--- docs/.vuepress/sidebar/extending_zh.js
+++ docs/.vuepress/sidebar/extending_zh.js
@@ -1,15 +1,15 @@
-module.exports = function() {
-    return[
+module.exports = function () {
+    return [
 
-{
-    title: 'LAMMPS客制化',
-    collapsable: true,
-    sidebarDepth: 3,
-    children: [
-        ['/zh/hello/customize/', '动手写自己的代码吧~'],
-        ['/zh/hello/customize/DIY_fix', '客制化fix'],
-        ['/zh/hello/customize/gdb', '使用gdb调试LAMMPS']
-    ]
-},
+        {
+            title: 'LAMMPS客制化',
+            collapsable: true,
+            sidebarDepth: 3,
+            children: [
+                ['/zh/extending/', '动手写自己的代码吧~'],
+                ['/zh/extending/DIY_fix', '客制化fix'],
+                ['/zh/extending/gdb', '使用gdb调试LAMMPS']
+            ]
+        },
     ]
 }
